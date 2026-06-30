727f062a203fe623fb49fd349af1cc06da8aa54e
Aniket Ramteke <aramteke870@gmail.com>
2026-06-30 13:09:23 +0530

    adding App.py file1

---
+++ app.py
@@ -0,0 +1,13 @@
+# Simple Python program
+
+def add_numbers(a, b):
+    return a + b
+
+# Main program
+print("Hello, World!")
+
+x = 10
+y = 20
+
+result = add_numbers(x, y)
+print("The sum is:", result)
