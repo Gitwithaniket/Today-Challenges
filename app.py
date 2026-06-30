10ef7cb27fce70c0bbadf22c0d07fedf568c2e8a
Aniket Ramteke <aramteke870@gmail.com>
2026-06-30 13:41:10 +0530

    This is Edit Code

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # Simple Python program
 
-def add_numbers(a, b):
-    return a + b
+def add_numbers(a, b, c):
+    return a + b + c
 
 # Main program
 print("Hello, World!")
 
 x = 10
 y = 20
+z = 30
 
-result = add_numbers(x, y)
+result = add_numbers(x, y, z)
 print("The sum is:", result)
